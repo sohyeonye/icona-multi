3a6944a74c8cea166b70971a75fb6d851b008190
sohyeonye <sohyeon.ye@wesang.com>
2025-07-11 10:38:13 +0900

    config update

--- icona.config.js
+++ icona.config.js
@@ -3,39 +3,71 @@ import core from "@actions/core";
 import { generate } from "@icona/generator";
 
 (async () => {
-    try {
-        const commitMessage = github.context.payload.head_commit?.message || '';
-        core.info('Commit message: ' + commitMessage);
+  try {
+    const commitMessage = github.context.payload.head_commit?.message || "";
+    core.info("Commit message: " + commitMessage);
 
-        const match = commitMessage.match(/Update (\.\/icona\/[^ ]+)/i);
-        if (!match) {
-            core.notice('No .icona/*.json file found in commit message');
-            return;
-        }
-
-        const filePath = match[1];
-        core.info('Found file path: ' + filePath);
-
-        const serviceName = filePath.replace('./icona/', '').replace('.json', '');
-        core.info('Extracted service name: ' + serviceName);
-
-        generate({
-            config: {
-              svg: {
-                genMode: "recreate", 
-                active: true, 
-                path: `${serviceName}/svg`, 
-                svgoConfig: {},
-              },
-              png: {
-                genMode: "recreate", 
-                active: true,
-                path: `${serviceName}/png`, // will generate png files in png folder
-              }
-            },
-            icons: `./.icona/${serviceName}.json` 
-          });
-    } catch (e) {
-        core.setFailed(e.message);
+    const match = commitMessage.match(/Update (\.\/icona\/[^ ]+)/i);
+    if (!match) {
+      core.notice("No .icona/*.json file found in commit message");
+      return;
     }
+
+    const filePath = match[1];
+    core.info("Found file path: " + filePath);
+
+    const serviceName = filePath.replace("./icona/", "").replace(".json", "");
+    core.info("Extracted service name: " + serviceName);
+
+    generate({
+      config: {
+        svg: {
+          genMode: "recreate",
+          active: true, // you can disable svg generator if you set false
+          path: "svg", // will generate svg files in svg folder
+          svgoConfig: {
+            plugins: [
+              {
+                name: "removeAttrs",
+                params: {
+                  attrs: ["id"],
+                },
+                fn: () => {
+                  return {
+                    element: {
+                      enter: (node) => {
+                        // NOTE: Not working
+                        // if (node.name === 'mask') return;
+
+                        // NOTE: Working
+                        if (node.name !== "mask") delete node.attributes.id;
+                      },
+                    },
+                  };
+                },
+              },
+              {
+                name: "convertColors",
+                params: { currentColor: true },
+              },
+              {
+                name: "prefixIds",
+                params: {
+                  prefix: `yds2-icon-`,
+                },
+              },
+            ],
+          },
+        },
+        png: {
+          genMode: "recreate",
+          active: true,
+          path: `${serviceName}/png`, // will generate png files in png folder
+        },
+      },
+      icons: `./.icona/${serviceName}.json`,
+    });
+  } catch (e) {
+    core.setFailed(e.message);
+  }
 })();
